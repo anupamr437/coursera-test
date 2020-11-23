c0ff17d9eab5fc33cf8aaf59d38780344a2a7242
anupamr437 <anupamr437@gmail.com>
2020-11-23 19:32:13 +0530

    final

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -34,7 +34,7 @@ WARNING!!! WARNING!!!
 // See Lecture 52, part 2
 // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-var names = ["Krishna", "Rama", "Jesus", "Buddha", "Mahavir", "Singh", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Kavita", "Rama", "John", "Krishna", "Mahavir", "Singh", "Luci", "Akshay", "Jed", "Jim"];
 
 // STEP 10:
 // Loop over the names array and say either 'Hello' or "Good Bye"
